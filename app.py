c0a28ad183a8b78e27df0ce2d25ef6a441b6951b
Nikita Korneev <60179299+NikitaKorneev@users.noreply.github.com>
2024-02-05 16:52:35 +0300

    start

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request
 import paho.mqtt.publish as publish
+import os
 
 app = Flask(__name__)
 
 MQTT_BROKER = "127.0.0.1"
 MQTT_PORT = 1883
 MQTT_TOPIC = "dahua2mqtt/"
-MQTT_USERNAME = "mqtt"
-MQTT_PASSWORD = "^QRLjWEsNq3Xg^hA"
+MQTT_USERNAME = os.getenv('OPTIONS_MQTT_USERNAME')
+MQTT_PASSWORD = os.getenv('OPTIONS_MQTT_PASSWORD')
 
 
 @app.route('/cgi-bin/NotifyEvent', methods=['POST'])
