581aff27d54c8e452bc039667f714672ce7206bb
Nikita Korneev <60179299+NikitaKorneev@users.noreply.github.com>
2024-02-06 23:35:15 +0300

    fixes

--- main/run.sh
+++ main/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 
 # Fetch the add-on options using the Supervisor API
 CONFIG=$(curl -s -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" http://supervisor/addons/self/options)
