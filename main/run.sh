d3311a3b50dee36ae246ca5cd6ba8f04c28a9d1e
Nikita Korneev <60179299+NikitaKorneev@users.noreply.github.com>
2024-02-07 00:05:16 +0300

    fixes

--- main/run.sh
+++ main/run.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/with-contenv bashio
-
-# Fetch the add-on options using the Supervisor API
-CONFIG=/main/config.yaml
-
-# Export the options as environment variables
-MQTT_BROKER="$(bashio::config 'mqtt_broker')"
-MQTT_PORT="$(bashio::config 'mqtt_port')"
-MQTT_USERNAME="$(bashio::config 'mqtt_username')"
-MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
-
-MQTT_TOPIC_FACE_RECOGNIZED="$(bashio::config 'mqtt_topic_face_recognized')"
-MQTT_TOPIC_FACE_STRANGER="$(bashio::config 'mqtt_topic_face_stranger')"
-MQTT_TOPIC_SMD_HUMAN="$(bashio::config 'mqtt_topic_smd_human')"
-MQTT_TOPIC_SMD_CAR="$(bashio::config 'mqtt_topic_smd_car')"
+#
+## Fetch the add-on options using the Supervisor API
+#CONFIG=/main/config.yaml
+#
+## Export the options as environment variables
+#MQTT_BROKER="$(bashio::config 'mqtt_broker')"
+#MQTT_PORT="$(bashio::config 'mqtt_port')"
+#MQTT_USERNAME="$(bashio::config 'mqtt_username')"
+#MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
+#
+#MQTT_TOPIC_FACE_RECOGNIZED="$(bashio::config 'mqtt_topic_face_recognized')"
+#MQTT_TOPIC_FACE_STRANGER="$(bashio::config 'mqtt_topic_face_stranger')"
+#MQTT_TOPIC_SMD_HUMAN="$(bashio::config 'mqtt_topic_smd_human')"
+#MQTT_TOPIC_SMD_CAR="$(bashio::config 'mqtt_topic_smd_car')"
 
 # Start the main Python application
 python /app/app.py
